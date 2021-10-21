acaddbfdc54d7f4c4c318d9f4958faffbcb0de3b
Stephen Koch <stephen.koch96@gmail.com>
2021-10-21 12:57:25 -0400

    initial commit

---
+++ src/FirstGUI/FirstGUI.java
@@ -0,0 +1,16 @@
+package FirstGUI;
+
+import javax.swing.*;
+
+/** @author sak5680 */
+public class FirstGUI {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(400,300);
+        frame.setLocation(200,200);
+        frame.setTitle("Empty Frame");
+        
+    }
+    
+}
